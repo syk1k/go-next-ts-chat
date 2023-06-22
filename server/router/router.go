add136bc4074a166deca21b5ff0eb371f3ea2627
syk1k <dambemondo@gmail.com>
2023-06-22 23:02:43 +0000

    Go chat

---
+++ server/router/router.go
@@ -0,0 +1,24 @@
+package router
+
+import (
+	"server/internal/user"
+	"server/internal/ws"
+
+	"github.com/gin-gonic/gin"
+)
+
+var r *gin.Engine
+
+func InitRouter(userHandler *user.Handler, wsHandler *ws.Handler) {
+	r = gin.Default()
+	r.POST("/signup", userHandler.CreateUser)
+	r.POST("/login", userHandler.Login)
+	r.GET("/logout", userHandler.Logout)
+
+	// WebSocket routers
+	r.POST("/ws/createRoom", wsHandler.CreateRoom)
+}
+
+func Start(addr string) error {
+	return r.Run(addr)
+}
